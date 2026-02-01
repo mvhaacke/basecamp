a7b1c26bbae26f635016e0833eaf947a8c6a8f1c
Moritz von Haacke <moritz@vhaacke.com>
2026-02-01 17:04:09 +0100

    Close db connections

--- src/basecamp/database.py
+++ src/basecamp/database.py
@@ -3,11 +3,12 @@ from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.pool import NullPool
 
 from basecamp.config import DATABASE_URL
 from basecamp.models import Base
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 SessionLocal = sessionmaker(bind=engine)
 
 
